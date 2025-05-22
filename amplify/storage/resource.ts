ba2383db318cfc01528f6a09c998a8e3b12d183a
masahiro <masahiro@masahironoMacBook-Pro.local>
2025-05-22 23:36:49 +0900

    add storage browser

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,11 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 'kodato-storagebrowser-test-0522',
+  access: (allow) => ({
+    'doc/*': [
+      allow.authenticated.to(['read','write', 'delete']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
+});
